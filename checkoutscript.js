5d3abcd9350b74555db71b7e150539593b2ee6af
krystallogistics <krystallogistics1@gmail.com>
2023-07-07 13:17:36 +0000

    New Commits

---
+++ checkoutscript.js
@@ -0,0 +1,22 @@
+    // Get the necessary elements
+    const campusSelect = document.getElementById('campus');
+    const priceDisplay = document.getElementById('price');
+
+    // Calculate the price based on the selected campus
+    function calculatePrice() {
+        const campusValue = campusSelect.value;
+        let price = 0;
+
+        if (campusValue === 'nyankpala') {
+            price = 12;
+        } else if (campusValue === 'dungu') {
+            price = 10;
+        } else if (campusValue === 'city-campus') {
+            price = 7;
+        }
+        // Display the price on the website
+        priceDisplay.textContent = `Your bill is: Ghc${price} `;
+    }
+
+    // Add event listener to the campus select element
+    campusSelect.addEventListener('change', calculatePrice);
