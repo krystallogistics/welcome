4426731b5b1f111823777c280789f95fe6ef6085
krystallogistics <krystallogistics1@gmail.com>
2023-07-07 17:25:01 +0000

    New Functionalities to Add Costs

---
+++ scripts/gasscript.js
@@ -0,0 +1,53 @@
+    // Get the necessary elements
+    const campusSelect = document.getElementById('campus');
+    const cylinderSelect = document.getElementById('cylinder-sizes');
+    const priceDisplay = document.getElementById('price');
+
+    // Calculate the price based on the selected campus and cylinder size
+    function calculatePrice() {
+        const campusValue = campusSelect.value;
+        const cylinderValue = cylinderSelect.value;
+
+        if (campusValue === 'nyankpala' && cylinderValue === 'big') {
+            totalPrice = 25; 
+        } else if (campusValue === 'nyankpala' && cylinderValue === 'mid'){
+            totalPrice = 20;
+        } 
+        else if (campusValue === 'nyankpala' && cylinderValue === 'small'){
+            totalPrice = 15;
+        }
+        else if (campusValue === 'dungu' && cylinderValue === 'big') {
+            totalPrice = 25;
+        } 
+        else if (campusValue === 'dungu' && cylinderValue === 'mid'){
+            totalPrice = 15;
+        } else if (campusValue === 'dungu' && cylinderValue === 'small'){
+            totalPrice = 15;
+        }
+        else if (campusValue === 'city-campus' && cylinderValue === 'big') {
+            totalPrice = 20;
+        } else if (campusValue === 'city-campus' && cylinderValue === 'mid') {
+            totalPrice = 15;
+        } 
+        else if (campusValue === 'city-campus' && cylinderValue === 'small') {
+            totalPrice = 10;
+        }
+
+        // if (cylinderValue === 'big') {
+        //     cylinderPrice = 20;
+        // } else if (cylinderValue === 'mid') {
+        //     cylinderPrice = 15;
+        // } else if (cylinderValue === 'small') {
+        //     cylinderPrice = 10;
+        // }
+
+        // // Calculate the total price
+        // const totalPrice = campusPrice + cylinderPrice;
+
+        // Display the total price on the website
+        priceDisplay.textContent = `Total Price: ${totalPrice} cedis`;
+    }
+
+    // Add event listeners to the select elements
+    campusSelect.addEventListener('change', calculatePrice);
+    cylinderSelect.addEventListener('change', calculatePrice);
